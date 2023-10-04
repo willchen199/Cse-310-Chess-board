fa4a2a79fe52c32d8558204d912c71bc4fb757c8
Sebastian Dunn <sebastiancdunn@byui.edu>
2023-10-04 09:12:02 -0600

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChessApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
